edb8e04cef6c54ac1ebd5e25d634b8c2f2328c72
Gisel Paye Luna <cecipaye@gmail.com>
2022-09-05 13:47:02 -0400

    insertar producto

--- npmtest/index.js
+++ npmtest/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT;
 //const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
@@ -17,6 +19,24 @@ app.get("/api/v1/products", (req, res) => {
     },
   });
 });
+
+app.post("/api/v1/products", (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/products.json`)
+    );
+  products.push(req.body);
+  fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
+  //console.log(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      products,
+    },
+  });
+});
+
+
+
 app.listen(port, () =>{
     console.log(`App running on port ${port}`);
 });
